4a11da2be7b4c28015e29a8d196fb8308a5f1c8f
ngp223 <ngomez@hi-iberia.es>
2026-06-24 14:53:37 +0200

    test autocontenido categories

--- features/backoffice/E2Ebo_07_categories.feature
+++ features/backoffice/E2Ebo_07_categories.feature
@@ -4,8 +4,10 @@ Feature: Categorías
     Given la web está abierta
     When hago login con credenciales válidas
 
-  Scenario: Crear una nueva categoría
+  Scenario: Crear y eliminar una categoría
 
     Then accedo a categorías
     And creo una nueva categoría
     And la categoría aparece en el listado
+    And elimino la categoría
+    And la categoría no aparece en el listado
--- features/backoffice/steps/E2Ebo_categories_steps.py
+++ features/backoffice/steps/E2Ebo_categories_steps.py
@@ -1,28 +1,34 @@
 from behave import then
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 from random import choice
+from selenium.webdriver.common.by import By
 from features.backoffice.pages.E2Ebo_categories_page import CategoriesPage_bo
 
 
 @then("accedo a categorías")
 def step_open_categories(context):
-    context.categories_page = CategoriesPage_bo(context.driver)
+    context.categories_page=CategoriesPage_bo(context.driver)
     context.categories_page.open_categories()
 
 
 @then("creo una nueva categoría")
 def step_create_category(context):
-    timestamp = datetime.now().strftime("%d%m%Y_%H%M%S")
-    tipo_categoria = choice(["Ensalada","Carne","Pescado","Pasta"])
-    context.category_name = (f"Categoría QA {tipo_categoria} {timestamp}")
+    timestamp=datetime.now().strftime("%d%m%Y_%H%M%S")
+    tipo=choice(["Ensalada","Carne","Pescado","Pasta"])
+    context.category_name=f"Categoría QA {tipo} {timestamp}"
     context.categories_page.create_category(context.category_name)
 
 
 @then("la categoría aparece en el listado")
 def step_check_category(context):
-    category_locator = (By.XPATH,f"//*[contains(text(),'{context.category_name}')]")
-    element = WebDriverWait(context.driver, 20).until(EC.visibility_of_element_located(category_locator))
-    assert element.is_displayed(), (f"No se encontró la categoría {context.category_name}")
+    assert context.categories_page.exists_item(context.category_name)
+
+
+@then("elimino la categoría")
+def step_delete_category(context):
+    context.categories_page.delete_category(context.category_name)
+
+
+@then("la categoría no aparece en el listado")
+def step_check_deleted_category(context):
+    context.categories_page.wait_item_gone(context.category_name)
