3ce595da40827b14267fca1b85eb310aca15ecd8
ngp223 <ngomez@hi-iberia.es>
2026-08-05 09:11:40 +0200

    corregido, test

--- features/mobile/pages/E2E_reservation_page.py
+++ features/mobile/pages/E2E_reservation_page.py
@@ -3,68 +3,61 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 class ReservationPage:
 
-    VENTAS_VIEW = ('xpath', "//*[contains(@text,'VENTAS')]")
-    TABLE_B1 = ('xpath', '//android.view.ViewGroup[@content-desc="B1"]')
-    GUEST = ('xpath', '//android.widget.TextView[@text="3"]')
-    ACCEPT_GUESTS = ('xpath', '//android.view.ViewGroup[@content-desc="Aceptar"]/android.view.ViewGroup')
-    PRODUCT_ARROZ_BOGAVANTE = (
-        'xpath',
-        '//android.view.ViewGroup[contains(@content-desc,"Arroz con Bogavante")]'
-    )
-    ADD_BUTTON = ('xpath', '//android.view.ViewGroup[@content-desc="Añadir al pedido"]/android.view.ViewGroup')
-    CART_BUTTON = ('xpath', '//android.widget.TextView[@text=""]/ancestor::android.view.ViewGroup[1]')
-    REALIZAR_PAGO_BUTTON = ('xpath', '//android.widget.TextView[@text="Realizar Pago"]')
-    CONFIRMAR_PAGO_BUTTON = ('xpath', '//android.widget.TextView[@text="Confirmar Pago"]')
-    FINALIZAR_BUTTON = ('xpath', '//android.widget.TextView[@text="Finalizar"]')
+    VENTAS_VIEW=(AppiumBy.XPATH,"//*[contains(@text,'VENTAS')]")
+    TABLE_b1=(AppiumBy.XPATH,'//android.view.ViewGroup[@content-desc="B1"]')
+    GUEST=(AppiumBy.ANDROID_UIAUTOMATOR,'new UiSelector().text("3").instance(1)')
+    ACCEPT_GUESTS=(AppiumBy.ANDROID_UIAUTOMATOR,'new UiSelector().text("Aceptar")')
+    PRODUCT_ARROZ_BOGAVANTE=(AppiumBy.XPATH,'//android.view.ViewGroup[contains(@content-desc,"Arroz con Bogavante")]')
+    ADD_BUTTON=(AppiumBy.XPATH,'//android.view.ViewGroup[@content-desc="Añadir al pedido"]/android.view.ViewGroup')
+    CART_BUTTON=(AppiumBy.XPATH,'//android.widget.TextView[@text=""]/ancestor::android.view.ViewGroup[1]')
+    REALIZAR_PAGO_BUTTON=(AppiumBy.XPATH,'//android.widget.TextView[@text="Realizar Pago"]')
+    CONFIRMAR_PAGO_BUTTON=(AppiumBy.XPATH,'//android.widget.TextView[@text="Confirmar Pago"]')
+    FINALIZAR_BUTTON=(AppiumBy.XPATH,'//android.widget.TextView[@text="Finalizar"]')
 
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self,driver):
+        self.driver=driver
 
-    def click(self, by, locator):
-        element = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((by, locator))
-        )
+    def click(self,by,locator):
+        element=WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((by,locator)))
         element.click()
 
-    def wait_quantity(self, qty, timeout=15):
-        xpath = f'//android.widget.TextView[@text="{qty}"]'
-        WebDriverWait(self.driver, timeout).until(
-            lambda d: len(d.find_elements(AppiumBy.XPATH, xpath)) > 0
-        )
+    def wait_quantity(self,qty,timeout=15):
+        xpath=f'//android.widget.TextView[@text="{qty}"]'
+        WebDriverWait(self.driver,timeout).until(lambda d:len(d.find_elements(AppiumBy.XPATH,xpath))>0)
         time.sleep(0.5)
 
     def select_table_b1(self):
-        self.click(AppiumBy.XPATH, self.TABLE_B1[1])
+        self.click(self.TABLE_b1[0],self.TABLE_b1[1])
 
     def select_guests(self):
-        self.click(AppiumBy.XPATH, self.GUEST[1])
+        self.click(self.GUEST[0],self.GUEST[1])
         time.sleep(2)
 
     def click_accept_guests(self):
-        self.click(AppiumBy.XPATH, self.ACCEPT_GUESTS[1])
+        self.click(self.ACCEPT_GUESTS[0],self.ACCEPT_GUESTS[1])
+        time.sleep(2)
 
     def select_product_arroz_bogavante(self):
-        self.click(AppiumBy.XPATH, self.PRODUCT_ARROZ_BOGAVANTE[1])
+        self.click(self.PRODUCT_ARROZ_BOGAVANTE[0],self.PRODUCT_ARROZ_BOGAVANTE[1])
 
     def click_add_product(self):
-        self.click(AppiumBy.XPATH, self.ADD_BUTTON[1])
+        self.click(self.ADD_BUTTON[0],self.ADD_BUTTON[1])
         self.wait_quantity("1")
 
     def increase_product(self):
-        self.click(AppiumBy.XPATH, self.CART_BUTTON[1])
+        self.click(self.CART_BUTTON[0],self.CART_BUTTON[1])
         self.wait_quantity("2")
-        self.click(AppiumBy.XPATH, self.CART_BUTTON[1])
+        self.click(self.CART_BUTTON[0],self.CART_BUTTON[1])
         self.wait_quantity("3")
         print("✅ Cantidad confirmada en 3")
 
     def click_realizar_pago(self):
-        self.click(AppiumBy.XPATH, self.REALIZAR_PAGO_BUTTON[1])
+        self.click(self.REALIZAR_PAGO_BUTTON[0],self.REALIZAR_PAGO_BUTTON[1])
 
     def click_confirmar_pago(self):
-        self.click(AppiumBy.XPATH, self.CONFIRMAR_PAGO_BUTTON[1])
+        self.click(self.CONFIRMAR_PAGO_BUTTON[0],self.CONFIRMAR_PAGO_BUTTON[1])
 
     def click_finalizar(self):
-        self.click(AppiumBy.XPATH, self.FINALIZAR_BUTTON[1])
+        self.click(self.FINALIZAR_BUTTON[0],self.FINALIZAR_BUTTON[1])
--- features/mobile/steps/E2E_reservation_steps.py
+++ features/mobile/steps/E2E_reservation_steps.py
@@ -2,53 +2,42 @@ from behave import when, then
 from features.mobile.pages.E2E_reservation_page import ReservationPage
 from features.mobile.pages.logout_page import LogoutPage
 
-
 @when("está en la pantalla de ventas")
 def step_impl(context):
-    context.reservation = ReservationPage(context.driver)
-
+    context.reservation=ReservationPage(context.driver)
 
 @when('selecciono la mesa "{table}"')
-def step_impl(context, table):
+def step_impl(context,table):
     context.reservation.select_table_b1()
-
 
 @when("selecciono comensales")
 def step_impl(context):
     context.reservation.select_guests()
     context.reservation.click_accept_guests()
 
-
 @when("selecciono el producto")
 def step_impl(context):
     context.reservation.select_product_arroz_bogavante()
-
-
 
 @when("añado el producto al pedido")
 def step_impl(context):
     context.reservation.click_add_product()
 
-
 @when("aumento la cantidad del producto")
 def step_impl(context):
     context.reservation.increase_product()
-
 
 @when("realizo el pago")
 def step_impl(context):
     context.reservation.click_realizar_pago()
 
-
 @when("confirmo el pago")
 def step_impl(context):
     context.reservation.click_confirmar_pago()
 
-
 @then("finalizo el pedido")
 def step_impl(context):
     context.reservation.click_finalizar()
-
 
 @then("salgo del modo TPV")
 def step_impl(context):
