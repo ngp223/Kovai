a67a509b54e1e7e302f093190e97379a2daa163f
ngp223 <ngomez@hi-iberia.es>
2026-08-12 10:41:53 +0200

    Corregido, error en comensales

--- features/mobile/pages/E2E_reservation_page.py
+++ features/mobile/pages/E2E_reservation_page.py
@@ -1,12 +1,12 @@
-import time
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 class ReservationPage:
     VENTAS_VIEW = (AppiumBy.XPATH, "//*[contains(@text,'VENTAS')]")
     TABLE_B1 = (AppiumBy.XPATH, '//android.view.ViewGroup[@content-desc="B1"]')
-    GUEST = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().description("3").instance(1)')
+    GUEST = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().description("3")')
     ACCEPT_GUESTS = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Aceptar")')
     ARROCES_DE_AUTOR = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Arroces de Autor")')
     PRODUCT_ARROZ_BOGAVANTE = (AppiumBy.XPATH, '//android.view.ViewGroup[contains(@content-desc,"Arroz con Bogavante")]')
@@ -19,60 +19,60 @@ class ReservationPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def wait_element(self, by, locator, timeout=30):
-        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((by, locator)))
+    def select_table_b1(self):
+        self.click(self.TABLE_B1)
 
-    def click(self, by, locator, timeout=30):
-        WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable((by, locator))).click()
+    def select_guests(self):
+        self.click(self.GUEST)
+
+    def click_accept_guests(self):
+        self.click(self.ACCEPT_GUESTS)
+
+    def select_product_arroz_bogavante(self):
+        self.click(self.ARROCES_DE_AUTOR)
+        self.wait_product_loaded()
+        self.click(self.PRODUCT_ARROZ_BOGAVANTE)
+        print("✅ Arroz con Bogavante seleccionado")
+
+    def click_add_product(self):
+        self.click(self.ADD_BUTTON)
+        self.wait_quantity("1")
+
+    def increase_product(self):
+        self.click(self.CART_BUTTON)
+        self.wait_quantity("2")
+        self.click(self.CART_BUTTON)
+        self.wait_quantity("3")
+        print("✅ Cantidad confirmada en 3")
+
+    def click_realizar_pago(self):
+        self.click(self.REALIZAR_PAGO_BUTTON)
+
+    def click_confirmar_pago(self):
+        self.click(self.CONFIRMAR_PAGO_BUTTON)
+
+    def click_finalizar(self):
+        self.click(self.FINALIZAR_BUTTON)
+
+    def wait_element(self, locator, timeout=30):
+        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def click(self, locator, timeout=30):
+        WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator)).click()
 
     def wait_quantity(self, qty, timeout=15):
         xpath = f'//android.widget.TextView[@text="{qty}"]'
-        WebDriverWait(self.driver, timeout).until(lambda d: len(d.find_elements(AppiumBy.XPATH, xpath)) > 0)
+        WebDriverWait(self.driver, timeout).until(lambda driver: len(driver.find_elements(AppiumBy.XPATH, xpath)) > 0)
         time.sleep(0.5)
 
     def wait_product_loaded(self, timeout=40):
         end = time.time() + timeout
         while time.time() < end:
             try:
-                products = self.driver.find_elements(self.PRODUCT_ARROZ_BOGAVANTE[0], self.PRODUCT_ARROZ_BOGAVANTE[1])
+                products = self.driver.find_elements(*self.PRODUCT_ARROZ_BOGAVANTE)
                 if products and products[0].is_displayed():
                     return True
             except Exception:
                 pass
             time.sleep(1)
         raise Exception("❌ Arroz con Bogavante no cargó")
-
-    def select_table_b1(self):
-        self.click(self.TABLE_B1[0], self.TABLE_B1[1])
-
-    def select_guests(self):
-        self.click(self.GUEST[0], self.GUEST[1])
-
-    def click_accept_guests(self):
-        self.click(self.ACCEPT_GUESTS[0], self.ACCEPT_GUESTS[1])
-
-    def select_product_arroz_bogavante(self):
-        self.click(self.ARROCES_DE_AUTOR[0], self.ARROCES_DE_AUTOR[1])
-        self.wait_product_loaded()
-        self.click(self.PRODUCT_ARROZ_BOGAVANTE[0], self.PRODUCT_ARROZ_BOGAVANTE[1])
-        print("✅ Arroz con Bogavante seleccionado")
-
-    def click_add_product(self):
-        self.click(self.ADD_BUTTON[0], self.ADD_BUTTON[1])
-        self.wait_quantity("1")
-
-    def increase_product(self):
-        self.click(self.CART_BUTTON[0], self.CART_BUTTON[1])
-        self.wait_quantity("2")
-        self.click(self.CART_BUTTON[0], self.CART_BUTTON[1])
-        self.wait_quantity("3")
-        print("✅ Cantidad confirmada en 3")
-
-    def click_realizar_pago(self):
-        self.click(self.REALIZAR_PAGO_BUTTON[0], self.REALIZAR_PAGO_BUTTON[1])
-
-    def click_confirmar_pago(self):
-        self.click(self.CONFIRMAR_PAGO_BUTTON[0], self.CONFIRMAR_PAGO_BUTTON[1])
-
-    def click_finalizar(self):
-        self.click(self.FINALIZAR_BUTTON[0], self.FINALIZAR_BUTTON[1])
